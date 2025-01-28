ae017a160514f00ba8184d60ed61bb5e2a3330fb
Batuhan Bartu Bebek <batuhanbartuu@gmail.com>
2025-01-29 01:19:46 +0300

    api fetch

--- personal-portfolio-website/src/context/LanguageContext.jsx
+++ personal-portfolio-website/src/context/LanguageContext.jsx
@@ -11,4 +11,20 @@ export const LanguageProvider = ({ children }) => {
   else{
     lang="tr"
   }
-  
+
+  const [language, setLanguage] = useState(lang);
+  const [translations,setTranslations]=useState({...languagesData});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTranslations = async () => {
+      try {
+        const response = await axios.post('https://reqres.in/api/workintech', languagesData[language]);
+        setTranslations(response.data);
+        if(!loading){
+          if(language==="tr"){
+            toast.success("Dil değişti.")
+          }else{
+            toast.success("Language changed.")
+          }
+        }      
