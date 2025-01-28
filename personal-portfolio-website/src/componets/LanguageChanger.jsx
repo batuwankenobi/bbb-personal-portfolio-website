82db0bb82dab549598146b5a440ea953b4c56812
Batuhan Bartu Bebek <batuhanbartuu@gmail.com>
2025-01-28 14:53:44 +0300

    language switcher

--- personal-portfolio-website/src/componets/LanguageChanger.jsx
+++ personal-portfolio-website/src/componets/LanguageChanger.jsx
@@ -2,7 +2,21 @@ function LanguageChanger() {
 	const { language, setLanguage, translate } = useLanguage();
 	const changeLanguage = translate("changeLanguage");
 	return (
-		<button></button>
-	)
-}
-	
+		<>
+		  <button className='font-semibold' onClick={() => { if (language === "tr") { setLanguage('en');  } else { setLanguage('tr');} }}>
+		    {language === "en" ? (
+			<>
+			  <span className="text-[#E92577]">{changeLanguage}</span><span className='text-[#777777]'>` YE GEÇ</span>
+			</>
+		    ) : (
+			<>
+			  <span className='text-[#777777]'>SWITCH TO</span> <span className="text-[#E92577]">{changeLanguage}</span>
+			</>
+		    )}
+	  
+		  </button>
+		</>
+	    )
+	  }
+	  
+	  export default LanguageChanger
