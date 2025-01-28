9d30879c5eacf9c970e6dc20da9a89b4336f415e
Batuhan Bartu Bebek <batuhanbartuu@gmail.com>
2025-01-28 17:14:55 +0300

    my data

---
+++ personal-portfolio-website/src/data.js
@@ -0,0 +1,17 @@
+const languagesData = {
+	en: {
+	    changeLanguage: "TÜRKÇE",
+	    hero: {
+		  greeting: "Hi! 👋",
+		  intro: " I'm a front-end developer. I can craft solid and scalable front-end products. Let's meet!",
+		  profileImg: "/assets/images/profile_picture.jpg",
+		  profileName: "I'm Batuhan.",
+		  linkedInSVG: "/assets/images/linkedIn.SVG",
+		  linkedInLink: "https://www.linkedin.com/in/batuhan-bartu-bebek/",
+		  githubSVG: "/assets/images/github.SVG",
+		  githubLink: "https://github.com/batuwankenobi",
+		  heroFooter: "Currently Freelancing for UX,UI, & Web Design Project.",
+		  heroFooter2: "Invite me to join your team -> ",
+		  heroFooterMail: "batuhanbartuu@gmail.com",
+	    },
+  
