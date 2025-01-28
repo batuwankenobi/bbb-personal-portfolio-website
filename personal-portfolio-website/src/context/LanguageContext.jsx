2db3ee45c138ec75540747ef9c0ed7527abe7842
Batuhan Bartu Bebek <batuhanbartuu@gmail.com>
2025-01-28 15:08:47 +0300

    language navigator

--- personal-portfolio-website/src/context/LanguageContext.jsx
+++ personal-portfolio-website/src/context/LanguageContext.jsx
@@ -1,3 +1,14 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+
+const LanguageContext = createContext();
+export const LanguageProvider = ({ children }) => {
+	var lang = navigator.language || navigator.userLanguage;
+  if(lang.includes("tr")){
+    lang="en"
+  }
+  else{
+    lang="tr"
+  }
+  
