8be94e54ad067bce71f1ffdcd6e288a04777be46
Batuhan Bartu Bebek <batuhanbartuu@gmail.com>
2025-01-30 14:42:34 +0300

    func exp

--- personal-portfolio-website/src/componets/ModeComponent.jsx
+++ personal-portfolio-website/src/componets/ModeComponent.jsx
@@ -1,6 +1,9 @@
-function ModeComponent() { 
-	const { translate } = useLanguage()
-	const lang = translate("changeLanguage");
+function ModeComponent() {
+	const { translate } = useLanguage(); // Çeviri fonksiyonunu almak için
+	const lang = translate("changeLanguage"); // Dilin değişim durumunu almak için
+    
+	// Tema bilgisini localStorage'da saklamak için özel hook kullanımı
 	const [dark, setDark] = useLocalStorage("theme", null);
+    
+	// Sayfa yüklendiğinde temayı localStorage'dan almak için useEffect kullanımı
 	useEffect(() => {
-		
