3383155419fe21b15832074efd2b39538175f654
Batuhan Bartu Bebek <batuhanbartuu@gmail.com>
2025-01-31 13:19:36 +0300

    explanatıon commıts added

--- personal-portfolio-website/src/componets/Profile.jsx
+++ personal-portfolio-website/src/componets/Profile.jsx
@@ -1,10 +1,17 @@
-import { useLanguage } from "../context/LanguageContext"; // Dil çeviri fonksiyonunu almak için
+// Dil çeviri fonksiyonunu kullanmak için gerekli bağlamı içe aktarıyoruz
+import { useLanguage } from "../context/LanguageContext";
+
 // Profil bileşeni
 function Profile() {
-	const { translate } = useLanguage(); // Dil çeviri fonksiyonunu kullanarak çevirileri alıyoruz
-	const profile = translate("profile"); // "profile" objesini çeviri dosyasından alıyoruz
-  
-	return (
-		<>	
-		<section className="bg-[#F4F4F4] dark:text-[#FFFFFF] dark:bg-[#2A262B]   py-24 content-center text-center"></section>
-		<div className="w-20 h-32  border-l-16 border-t-16 border-b-16 border-solid border-[#EA2678]  rounded-l-full  place-self-end  -mt-36"></div>
+    // useLanguage hook'unu kullanarak dil çeviri fonksiyonunu alıyoruz
+    const { translate } = useLanguage();
+    // "profile" çeviri verisini alıyoruz
+    const profile = translate("profile");
+
+    return (
+        <>
+            {/* Profil sayfası ana bölümü */}
+            <section className="bg-[#F4F4F4] dark:text-[#FFFFFF] dark:bg-[#2A262B] py-24 content-center text-center">
+                
+                {/* Dekoratif sol çizgi (pembe renkli) */}
+                <div className="w-20 h-32 border-l-16 border-t-16 border-b-16 border-solid border-[#EA2678] rounded-l-full place-self-end -mt-36"></div>
