ddf64d77e7bff47651948db25e973f47da76a5ba
Batuhan Bartu Bebek <batuhanbartuu@gmail.com>
2025-01-31 13:17:31 +0300

    UI ELEMTS

--- personal-portfolio-website/src/componets/Profile.jsx
+++ personal-portfolio-website/src/componets/Profile.jsx
@@ -7,3 +7,4 @@ function Profile() {
 	return (
 		<>	
 		<section className="bg-[#F4F4F4] dark:text-[#FFFFFF] dark:bg-[#2A262B]   py-24 content-center text-center"></section>
+		<div className="w-20 h-32  border-l-16 border-t-16 border-b-16 border-solid border-[#EA2678]  rounded-l-full  place-self-end  -mt-36"></div>
