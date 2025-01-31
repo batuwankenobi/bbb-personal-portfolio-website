57968d08dbb24f9058ec9e17852951d4cfc792ef
Batuhan Bartu Bebek <batuhanbartuu@gmail.com>
2025-01-31 13:16:46 +0300

    mobile screen adjustments

--- personal-portfolio-website/src/componets/Profile.jsx
+++ personal-portfolio-website/src/componets/Profile.jsx
@@ -4,4 +4,6 @@ function Profile() {
 	const { translate } = useLanguage(); // Dil çeviri fonksiyonunu kullanarak çevirileri alıyoruz
 	const profile = translate("profile"); // "profile" objesini çeviri dosyasından alıyoruz
   
-	return (	
+	return (
+		<>	
+		<section className="bg-[#F4F4F4] dark:text-[#FFFFFF] dark:bg-[#2A262B]   py-24 content-center text-center"></section>
