5b6f6134c8c594dc62b40203980c078913d3e405
Batuhan Bartu Bebek <batuhanbartuu@gmail.com>
2025-02-01 18:36:43 +0300

    ceviriyi alıyorum ve sabite atıyoruz

--- personal-portfolio-website/src/componets/Footer.jsx
+++ personal-portfolio-website/src/componets/Footer.jsx
@@ -1,1 +1,10 @@
 import { useLanguage } from "../context/LanguageContext";
+
+function Footer() {
+	// useLanguage hook'u ile çeviri fonksiyonunu alıyoruz
+	const { translate } = useLanguage();
+	// Footer içeriğini çeviri dosyasından alıyoruz
+	const footer = translate("footer");
+	
+	return (
+		
