e30bbac9746d4a1ece38106c7fe1768c2f15de80
Batuhan Bartu Bebek <batuhanbartuu@gmail.com>
2025-02-01 18:22:08 +0300

    key yardımıyla dizi çekme

--- personal-portfolio-website/src/componets/Skills.jsx
+++ personal-portfolio-website/src/componets/Skills.jsx
@@ -1,6 +1,16 @@
 import { useLanguage } from "../context/LanguageContext";
 
 function Skills() {
+	// useLanguage fonksiyonunu kullanarak translate fonksiyonunu elde ediyoruz.
+    // translate, verilen anahtar değerine göre uygun dildeki metni veya veriyi döndürür.
+    const { translate } = useLanguage();
+    
+    // "skills" anahtarını kullanarak skills adlı nesneyi alıyoruz.
+    // Bu nesne içerisinde başlık (h1) ve image bilgileri (images) bulunuyor.
+    const skills = translate("skills");
+    
+    // skills nesnesinden images dizisini alıyoruz.
+    const images = skills.images;
 	return (
 
 	);
